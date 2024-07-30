88331ac8e0004fb445098dfb2793ab370198b82f
arthu <arthu@ARTHUR>
2024-07-29 21:23:10 -0300

    Adicionar arquivos de projeto.

---
+++ Switch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cor = "verde";
+
+            switch(cor){
+                case "Vermelho":
+                    Console.WriteLine("Sua cor favorita é vermelha");
+                    break;
+                case "Amarelo":
+                    Console.WriteLine("Sua cor favorita é amarelo");
+                    break;
+
+                default:
+                    Console.WriteLine("sua cor nao foi encontrada");
+                    break;
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+
+
+
+    }
+}
